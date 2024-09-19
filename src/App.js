947ab65c62e488b0d18e720116202c0d332967a5
lbruesehoff <lbruesehoff@gmail.com>
2024-09-18 20:15:25 -0500

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import NavBar from './components/nav-bar/nav-bar.component.tsx';
-
+import "./App.css";
+import NavBar from "./components/nav-bar/nav-bar.component.tsx";
+import Hero from "./components/hero-section/hero.component.tsx";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Hero></Hero>
     </div>
   );
 }
