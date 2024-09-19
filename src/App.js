0aac508f19d78d37232b3144b12612e3b257d448
lbruesehoff <lbruesehoff@gmail.com>
2024-09-18 20:33:04 -0500

    Add footer section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/nav-bar/nav-bar.component.tsx";
 import Hero from "./components/hero-section/hero.component.tsx";
+import Footer from "./components/footer/footer.component.tsx";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Hero></Hero>
+      <Footer></Footer>
     </div>
   );
 }
