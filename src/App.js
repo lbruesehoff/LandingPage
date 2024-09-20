7f692edd2b777031b7888662526ceab594dc4e32
lbruesehoff <lbruesehoff@gmail.com>
2024-09-20 18:48:15 -0500

    set up feature section with card component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import NavBar from "./components/nav-bar/nav-bar.component.tsx";
 import Hero from "./components/hero-section/hero.component.tsx";
 import Footer from "./components/footer/footer.component.tsx";
+import FeatureSection from "./components/feature-section/feature.component.tsx";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Hero></Hero>
+      <FeatureSection></FeatureSection>
       <Footer></Footer>
     </div>
   );
---
+++ src/components/feature-section/feature.component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DynamicCard from "../dynamic-card/dynamic-card.component";
+
+const FeatureSection = () => {
+  return (
+    <div>
+      <div>
+        <DynamicCard />
+      </div>
+    </div>
+  );
+};
+
+export default FeatureSection;
