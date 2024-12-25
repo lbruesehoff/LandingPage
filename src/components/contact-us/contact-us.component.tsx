c6de9ebbde6bb535ec780d483db187a457192a42
lbruesehoff <lbruesehoff@gmail.com>
2024-12-25 11:57:25 -0600

    update visuals

--- src/components/contact-us/contact-us.component.tsx
+++ src/components/contact-us/contact-us.component.tsx
@@ -5,7 +5,7 @@ import "./contact-us.component.scss";
 const ContactUs = () => {
   return (
     <div className="hero bg-base-200 min-h-screen contact-us" id="contact-us">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="contact-us-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Contact Us!</h1>
           <p className="py-6">
